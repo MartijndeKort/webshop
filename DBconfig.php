d0c78430f43fcd042ee305be62a091380c07faf9
MartijndeKort <565343@edu.rocmn.nl>
2022-05-18 15:19:08 +0200

    heb alles aangepast en toegevoegd

--- DBconfig.php
+++ DBconfig.php
@@ -1,10 +1,10 @@
 <?php
 DEFINE("USER", "root");
-DEFINE("PASSWORD", "root");
+DEFINE("PASSWORD", "");
 
 try {
     $verbinding = new
-    PDO("mysql:host=localhost;dbname=webshop", USER_PASSWORD);
+    PDO("mysql:host=localhost;dbname=webshop", USER,PASSWORD);
     $verbinding->setAttribute
     (PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
 }catch(PDOException $e) {
